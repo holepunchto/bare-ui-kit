f1b1b449836000c4072527922339b8b8476a0f83
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-05-29 13:30:31 +0200

    Add Lunte, publish workflow, and use `bare-build` v1

--- runtime.js
+++ runtime.js
@@ -1,3 +1,7 @@
+const { type } = require('bare-build/constants')
+
+const { EXECUTABLE } = type
+
 require.asset = require('require-asset')
 
 const prebuilds = {}
@@ -5,13 +9,25 @@ const prebuilds = {}
 exports.prebuilds = prebuilds
 
 prebuilds['ios-arm64'] = () => {
-  return require.asset('./prebuilds/ios-arm64/bare', __filename)
+  return {
+    type: EXECUTABLE,
+    path: require.asset('./prebuilds/ios-arm64/bare', __filename),
+    dependencies: []
+  }
 }
 
 prebuilds['ios-arm64-simulator'] = () => {
-  return require.asset('./prebuilds/ios-arm64-simulator/bare', __filename)
+  return {
+    type: EXECUTABLE,
+    path: require.asset('./prebuilds/ios-arm64-simulator/bare', __filename),
+    dependencies: []
+  }
 }
 
 prebuilds['ios-x64-simulator'] = () => {
-  return require.asset('./prebuilds/ios-x64-simulator/bare', __filename)
+  return {
+    type: EXECUTABLE,
+    path: require.asset('./prebuilds/ios-x64-simulator/bare', __filename),
+    dependencies: []
+  }
 }
